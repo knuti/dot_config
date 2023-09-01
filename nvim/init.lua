06fcc65275c11bd50d3eaf31d06d0f85e2e958df
Axel Yoshimoto <240724+knuti@users.noreply.github.com>
2023-09-01 15:03:23 +0200

    some awesome keymappings i need

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,44 @@ vim.g.maplocalleader = ' '
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- [[ for a keymaps file ]]
+local opt = vim.opt
+opt.showmode = false
+opt.expandtab = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.signcolumn = "yes"
+opt.termguicolors = true
+
+local km = vim.keymap
+
+local opts = { noremap = true, silent = true }
+-- local term_opts = { silent = true }
+-- -- Shorten function name
+-- local keymap = vim.api.nvim_set_keymap
+
+opts.desc = "clear highlights from search"
+km.set('n', '<Esc>', ":noh <CR>", opts)
+
+opts.desc = "resize in the arrows direction"
+km.set("n", "<C-Up>", ":resize -2<CR>", opts)
+km.set("n", "<C-Down>", ":resize +2<CR>", opts)
+km.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+km.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+opts.desc = "open a new tab"
+km.set("n", "<leader>t", "<cmd> tabnew <CR>", opts)
+
+opts.desc = "close a tab"
+km.set("n", "<leader>c", "<cmd> close <CR>", opts)
+
+opts.desc = "quick save"
+km.set("n", "<leader>w", "<cmd> w <CR>", opts)
+
+opts.desc = "navigate between tabs with [SHIFT] TAB"
+km.set("n", "<tab>", "<cmd> tabnext <CR>", opts)
+km.set("n", "<S-tab>", "<cmd> tabprevious <CR>", opts)
+
 -- finally, a package manager
 -- github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
