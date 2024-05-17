99781061699e31cec2153a3bb557815d0ca993a0
Axel Yoshimoto <240724+knuti@users.noreply.github.com>
2024-05-17 13:18:14 +0200

    clean up comments

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,30 +15,14 @@ return {
           path_display = { "smart" },
         },
       })
-      --     mappings = {
-      --       i = {
-      --         ["<C-k>"] = actions.move_selection_previous, --move to previous result
-      --         ["<C-j>"] = actions.move_selection_next, --move to next result
-      --         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, --open result list in quick-fix-menu
-      --       },
-      --     },
-      --   },
-      -- })
 
       telescope.load_extension("fzf")
-
-      -- local builtin = require("telescope.builtin")
-      --
-      -- km.set("n", "<c-p>", builtin.find_files, {})
-      -- km.set("n", "<SPACE><SPACE>", builtin.oldfiles, { desc = "oldfiles?!" })
-      -- km.set("n", "<SPACE>fg", builtin.live_grep, { desc = "grep files as you type" })
-      -- km.set("n", "<SPACE>fh", builtin.help_tags, { desc = "help_tags?!" })
 
       local km = vim.keymap
       km.set("n", "<c-p>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy finding of files" })
       km.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy finding of files" })
       km.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy finding of most recent files" })
-      km.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in current working Dir" })
+      km.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in current working Dir" })
       km.set("n", "<leader>fc", "<cmd>Telescope greo_string<cr>", { desc = "Find string under cursor in cwd" })
       km.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "help_tags?!" })
       km.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
