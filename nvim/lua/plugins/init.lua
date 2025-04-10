636cb67c6b2fb74cab47825caa5ccba0cfa3416e
Axel Yoshimoto <240724+knuti@users.noreply.github.com>
2025-04-10 07:02:07 +0200

    explicitly configure tmux navigator

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,4 @@
 -- list of plugins that can just be installed
 return {
-	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
-	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
 }
